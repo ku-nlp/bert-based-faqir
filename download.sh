68d4a3d066bed15e06376d55ae20978f4b0de327
Sakata Wataru <>
2019-07-03 10:10:11 +0900

    small fix

--- download.sh
+++ download.sh
@@ -2,8 +2,8 @@ mkdir ./data
 cd data
 git clone https://github.com/google-research/bert.git
 #For Japanese, comment out text = self._tokenize_chinese_chars(text) in tokenization.py
-curl http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/JapaneseBertPretrainedModel/Japanese_L-12_H-768_A-12_E-30_BPE.zip > Japanese_L-12_H-768_A-12_E-30_BPE.zip.zip
-unzip Japanese_L-12_H-768_A-12_E-30_BPE.zip.zip
+curl http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/JapaneseBertPretrainedModel/Japanese_L-12_H-768_A-12_E-30_BPE.zip > Japanese_L-12_H-768_A-12_E-30_BPE.zip
+unzip Japanese_L-12_H-768_A-12_E-30_BPE.zip
 curl https://tulip.kuee.kyoto-u.ac.jp/localgovfaq/localgovfaq.zip > localgovfaq.zip
 unzip localgovfaq.zip
 cd -
